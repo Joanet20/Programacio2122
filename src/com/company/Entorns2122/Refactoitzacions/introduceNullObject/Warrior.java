9ee9542df2cf038dab6a69f1603e99a7617d4ad3
Joan Vallbona <johnyvallbona@gmail.com>
2022-03-21 11:04:27 +0100

    Refactoritzacions 17-22

---
+++ src/com/company/Entorns2122/Refactoitzacions/introduceNullObject/Warrior.java
@@ -0,0 +1,2 @@
+package com.company.Entorns2122.Refactoitzacions.introduceNullObject;public class Warrior {
+}
---
+++ src/com/company/Entorns2122/Refactoitzacions/replaceConditionalsWithPolymorphism/Plane.java
@@ -0,0 +1,2 @@
+package com.company.Entorns2122.Refactoitzacions.replaceConditionalsWithPolymorphism;public class Plane {
+}
